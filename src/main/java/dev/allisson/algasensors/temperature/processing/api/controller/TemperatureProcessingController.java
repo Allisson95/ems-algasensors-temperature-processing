140b777fea8756dd3d767ecefdb85bbe7ffdf980
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-04-28 22:50:44 -0300

    refactor TemperatureProcessingController to improve message processing and enhance readability

--- src/main/java/dev/allisson/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/dev/allisson/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -1,16 +1,21 @@
 package dev.allisson.algasensors.temperature.processing.api.controller;
 
-import dev.allisson.algasensors.temperature.processing.api.model.TemperatureLog;
-import io.hypersistence.tsid.TSID;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import static dev.allisson.algasensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfiguration.FANOUT_EXCHANGE_NAME;
+import dev.allisson.algasensors.temperature.processing.api.model.TemperatureLog;
+import dev.allisson.algasensors.temperature.processing.infrastructure.rabbitmq.RabbitMQConfiguration;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,7 +26,7 @@ public class TemperatureProcessingController {
     private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
-    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+    public void data(@PathVariable final TSID sensorId, @RequestBody final String input) {
         log.info("Received temperature data for sensor {}: {}", sensorId, input);
         if (input == null || input.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
@@ -30,14 +35,19 @@ public class TemperatureProcessingController {
         double temperature;
         try {
             temperature = Double.parseDouble(input);
-        } catch (NumberFormatException e) {
+        } catch (final NumberFormatException e) {
             log.warn("Error parsing temperature data for sensor {}: {}", sensorId, input);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
         final TemperatureLog temperatureLog = new TemperatureLog(sensorId, temperature);
 
-        this.rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, "", temperatureLog);
+        final MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", sensorId.toString());
+            return message;
+        };
+
+        this.rabbitTemplate.convertAndSend(RabbitMQConfiguration.FANOUT_EXCHANGE_NAME, "", temperatureLog, messagePostProcessor);
         log.trace("Temperature data sent to processing: {}", temperatureLog);
     }
 
